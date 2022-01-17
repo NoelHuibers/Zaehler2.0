fc0f7ebe092eedf4120cc9b87ed0244128f490bc
Noel Huibers <noel@huibers.io>
2022-01-17 14:21:12 +0100

    Commenting the Code
    Commenting the Code

--- app/src/main/java/com/noelHuibers/counterapp/Storage/SharedPrefManager.java
+++ app/src/main/java/com/noelHuibers/counterapp/Storage/SharedPrefManager.java
@@ -3,17 +3,34 @@ package com.noelHuibers.counterapp.Storage;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-
+/**
+ * Die Klasse SharedPrefManager findet die Nutzereinstellungen des genutzen Android Gerätes heraus und stellt die App dementsprechend standartmäßig auf Dark- oder Whitemode.
+ *
+ * @author Noel Huibers
+ * @version 2.0.0
+ */
 public class SharedPrefManager {
 
+    //Class Variables
     private static final String SHARED_PREF_NAME = "my_shared_preff";
     public static SharedPrefManager mInstance;
     private static Context mCtx;
 
+    /**
+     * Diese ist der Konstruktor der Klasse SharedPrefManager. Diese Funktion wird beim erstellen des Objekts ausgeführt und setzt die Contextvariable des Nutzers.
+     * @param name;
+     * @ensures this.name = name;
+     */
     private SharedPrefManager(Context mCtx) {
         this.mCtx = mCtx;
     }
 
+    /**
+     * Diese Methode synchronisiert die Instanz des Objekts.
+     * @param mCtx;
+     * @ensures mInstance = new SharedPrefManager(mCtx)
+     * @returns mInstance;
+     */
     public static synchronized SharedPrefManager getInstance(Context mCtx) {
         if (mInstance == null) {
             mInstance = new SharedPrefManager(mCtx);
@@ -21,20 +38,33 @@ public class SharedPrefManager {
         return mInstance;
     }
 
-
+    /**
+     * Diese Methode checkt ob der Nutzer eingeloggt ist und die Einstellung damit abrufbar
+     * @returns sharedPrefrences.getString();
+     */
     public boolean isLoggedIn(){
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
         return sharedPreferences.getString("name",null) != null;
     }
 
+    /**
+     * Diese Methode cleared die gespeicherten sharedPrefrences.
+     * @ensures SharedPrefrences.editor.clear()
+     * @ensures SharedPrefrences.editor.apply()
+     */
     public void clear() {
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.clear();
         editor.apply();
     }
-
-
+    
+   /**
+    * Diese Methode speichert den Startmodus der App zu jeweils Dark- oder Whitemode je nach Präferenz in den Nutzereinstellungen.
+    * @param darkMode
+    * @ensures SharedPrefrences.editor.putBoolean("Mode", darkmode)
+    * @ensures SharedPrefrences.editor.apply()
+    */
     public void saveMode(boolean darkMode) {
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -42,11 +72,12 @@ public class SharedPrefManager {
         editor.apply();
     }
 
+    /**
+     * Diese Methode ist die getterMethode um den bevorzugten Anzeigemodus zu bekommen
+     * @return sharedPrefrences.getBoolean("Mode", true);
+     */
     public boolean getMode() {
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean("Mode", true);
     }
-
-
-
 }
